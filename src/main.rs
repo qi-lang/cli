cc195d5f5260a3a93063fd4c3a0b1d0587aaa67b
zana-a <zana200.a@gmail.com>
2021-10-04 16:06:12 +0100

    fix path for inner core

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use qi_core;
 
 fn main() {
-    let result = qi_core::parse_from_file(
-        "C:\\Users\\Zana\\Documents\\code\\qi\\core\\demo_ignore\\playground\\other.qi",
-        qi_core::parsers::atom::parse,
-    );
+    let result = qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::atom::parse);
 
     println!("{:?}", result)
 }
