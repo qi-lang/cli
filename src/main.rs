04a3611f9cb38b0a4c28c7eada0035721fddff0c
zana-a <zana200.a@gmail.com>
2021-10-04 16:11:58 +0100

    add readme and update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use qi_core;
 fn main() {
     let result = qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::atom::parse);
 
-    println!("{:?}", result)
+    println!("{:#?}", result)
 }
