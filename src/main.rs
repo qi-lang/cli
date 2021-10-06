0badfdc9924b18fcce3a7a57b898e52ac8de0269
zana-a <zana200.a@gmail.com>
2021-10-06 10:46:02 +0100

    push all

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+/*
+ * Copyright Qi Lang. 2021 All Rights Reserved.
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
 use qi_core;
 
 fn main() {
-    let result = qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::atom::parse);
-
-    println!("{:#?}", result)
+    println!(
+        "{:?}",
+        qi_core::parse_from_str("[]", qi_core::parsers::list::parse)
+    );
+    println!(
+        "{:?}",
+        qi_core::parse_from_str("[true, false]", qi_core::parsers::list::parse)
+    );
+    println!(
+        "{:?}",
+        qi_core::parse_from_str("[true, false, true]", qi_core::parsers::list::parse)
+    );
 }
