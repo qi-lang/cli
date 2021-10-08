16a662f4d24d8d8e0119559bc9411d5a865e62ff
zana-a <zana200.a@gmail.com>
2021-10-08 14:28:53 +0100

    add the general parser for definition

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use qi_core;
 
 fn main() {
     let earlier = std::time::Instant::now();
-    let result = qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::map::parse);
+    let result =
+        qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::definition::parse);
     let later = std::time::Instant::elapsed(&earlier);
 
     println!("Time to complete: {:#?}", later);
