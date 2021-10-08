dd2a6da1833fc377900fa6edfbf157f72cf038eb
zana-a <zana200.a@gmail.com>
2021-10-08 15:18:00 +0100

    add module parser - incomplete

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,18 @@
 use qi_core;
 
 fn main() {
+    // let earlier = std::time::Instant::now();
+    // let result =
+    //     qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::definition::parse);
+    // let later = std::time::Instant::elapsed(&earlier);
+
+    // println!("Time to complete: {:#?}", later);
+    // println!("Result: {:#?}", result);
+
+    // println!("--------------------------------");
+
     let earlier = std::time::Instant::now();
-    let result =
-        qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::definition::parse);
+    let result = qi_core::parse_from_file("./playground/other.qi", qi_core::parsers::module::parse);
     let later = std::time::Instant::elapsed(&earlier);
 
     println!("Time to complete: {:#?}", later);
