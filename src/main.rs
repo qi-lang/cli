46c7fbea4ab7e55c217f95cd77a328cdbcec35f8
zana-a <zana200.a@gmail.com>
2022-02-04 22:07:49 +0000

    test modules

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-/*
- * Copyright Qi Lang. 2021 All Rights Reserved.
- * This file is licensed under the MIT License.
- * License text available at https://opensource.org/licenses/MIT
- */
-
-use qi_core::parsers;
-use qi_interactive;
-
 fn main() {
-    qi_interactive::init(parsers::module::parse);
+    println!("{}", qi_core::hello());    
+    println!("{}", qi_interactive::INTERACTIVE); 
 }
